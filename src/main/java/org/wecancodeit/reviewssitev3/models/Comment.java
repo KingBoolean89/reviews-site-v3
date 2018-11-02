599a05305d434ff8b41a9cf3858a45c6915faaf0
KingBoolean89 <aking893.ak@gmail.com>
2018-11-02 11:52:21 -0400

    Controllers

---
+++ src/main/java/org/wecancodeit/reviewssitev3/models/Comment.java
@@ -0,0 +1,44 @@
+package org.wecancodeit.reviewssitev3.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+public class Comment {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String commentName;
+	private String commentContent;
+	@ManyToOne
+	private Review review;
+	
+
+	public Comment() {
+		
+	}
+
+	public Comment(Long id, String commentName, String commentContent) {
+		super();
+		this.id = id;
+		this.commentName = commentName;
+		this.commentContent = commentContent;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getCommentName() {
+		return commentName;
+	}
+
+	public String getCommentContent() {
+		return commentContent;
+	}
+	
+	public Review getReview() {
+		return review;
+	}
+
+}
