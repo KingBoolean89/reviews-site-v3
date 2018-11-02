b3026ec8f6153975b47cf269d5061ebb8228d9ce
Alexander Greene <greene.alexanderj@gmail.com>
2018-11-02 10:21:05 -0400

    Initial commit

---
+++ src/main/java/org/wecancodeit/reviewssitev3/ReviewsSiteV3Application.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.reviewssitev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsSiteV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsSiteV3Application.class, args);
+	}
+}
